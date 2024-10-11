377156ab97470001d260b26ee71284ddeea67795
guanzhiq <guanzhiq@students.cs.ubc.ca>
2024-10-10 22:53:19 -0700

    Created Enemy class in model

---
+++ src/main/model/Enemy.java
@@ -0,0 +1,34 @@
+package model;
+
+
+/*
+ * Represent an enemy in game, with
+ * a position, health and carried item
+ */
+
+public class Enemy {
+
+    //Effects: constructs a enemy at position (x,y), 
+    //with random item carried
+    public Enemy() {
+        //stub
+    }
+
+    //EFFECTS: enemy move randomly, change position
+    public void move() {
+
+    }
+
+    //EFFECTS: enemy health decreases when attacked
+    public void takeDamage(int damage) {
+
+    }
+
+    //EFFECTS: enemy drop the item it carries
+    public Item dropItem() {
+        return Item;
+    }
+
+    //Setter Getter
+
+}
