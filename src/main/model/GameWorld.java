9e658114a36a9993ce4337ce3ee370388ca0fa32
guanzhiq <guanzhiq@students.cs.ubc.ca>
2024-10-10 23:27:41 -0700

    Created GameWorld class in model

---
+++ src/main/model/GameWorld.java
@@ -0,0 +1,45 @@
+package model;
+
+/*
+ * Represent a demon survivor game with a player, list of enemies,
+ * list of items, and grid size(x by y)
+ */
+
+public class GameWorld {
+
+    //EFFECTS: construct a gameworld with no enemy or item, a size of colum x row,
+    //and player at 0,0
+    public GameWorld(int colum, int row) {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: update player, enemies and items
+    public void update() {
+
+    }
+
+    //EFFECTS: check if the game is over
+    public void checkGameOver() {
+
+    }
+
+    //EFFECTS:move enemies
+    public void moveEnemies() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: remove enemies that has no health
+    public void checkEnemies() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: remove items picked up by player
+    public void checkItems() {
+
+    }
+
+    
+}
