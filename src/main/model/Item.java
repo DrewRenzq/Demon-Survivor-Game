d7e398e3e09589a07e0b0db1f1f78522b0a23beb
guanzhiq <guanzhiq@students.cs.ubc.ca>
2024-10-10 23:11:56 -0700

    Created Item class in model

---
+++ src/main/model/Item.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Random;
+
+/*
+ * Represent a item with its position and effect(type:integer, value:integer)
+ */
+
+public class Item {
+
+    //EFFECTS: construct an item with random effect
+    public Item() {
+
+    }
+
+    //Getter
+    public String getType() {
+        return "a";
+    }
+
+    public int getValue() {
+        return 1;
+    }
+
+
+}
