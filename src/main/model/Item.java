825686c80f4d2863932a9af82211b592b9ca3e4b
guanzhiq <guanzhiq@students.cs.ubc.ca>
2024-10-11 12:51:36 -0700

    Added field name in Item

--- src/main/model/Item.java
+++ src/main/model/Item.java
@@ -9,14 +9,17 @@ import java.util.Random;
 public class Item {
     private int x;
     private int y;
+    private String name;
     private int type;
     private int value;
+    String[] possibleNames = {"Potion", "Sword", "Helmet","Armor","Amulet","Ring"};
 
-    //EFFECTS: construct an item with random effect
+    //EFFECTS: construct an item with a random name and effect
     public Item() {
         Random random = new Random();
-        int type = random.nextInt(2); // 2 types: health or attack
-        int value = random.nextInt(11); // 0 - 10
+        this.name = possibleNames [random.nextInt(possibleNames.length)];
+        this.type = random.nextInt(3); // 3 types: heal, increase max health or attack
+        this.value = random.nextInt(11); // 0 - 10
 
     }
 
@@ -27,7 +30,7 @@ public class Item {
         this.y = y;
     }
 
-    //Getter
+    //Getter Setter
     public int getType() {
         return this.type;
     }
@@ -42,6 +45,14 @@ public class Item {
 
     public int getY() {
         return this.y;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public String[] getNames() {
+        return possibleNames;
     }
 
 
